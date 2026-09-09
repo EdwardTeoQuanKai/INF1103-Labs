d02d7a99f54791040a4e263f39ba220a863400bb
EdwardTeo <2603728@sit.singaporetech.edu.sg>
2026-09-09 15:15:37 +0800

    First Post

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 # Activity 1
-# print("================================")
-# print("Welcome Here")
-# print("My first post!")
-# print("================================")
+print("================================")
+print("Welcome Here")
+print("My first post!")
+print("================================")
 
 # Activity 2
 # print("================================")
@@ -19,16 +19,16 @@
 # print("Followers:", followers)
 
 # Activity 3
-followers = 100
+# followers = 100
 
-followers += 50
-print("Day 1:", followers)
+# followers += 50
+# print("Day 1:", followers)
 
-followers += 20
-print("Day 2:", followers)
+# followers += 20
+# print("Day 2:", followers)
 
-followers -= 10
-print("Day 3:", followers)
+# followers -= 10
+# print("Day 3:", followers)
 
 
 # Activity 4
