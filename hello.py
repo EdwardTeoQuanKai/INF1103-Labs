a07b0511c6ec92c64f9d8118040a375d02d4d631
EdwardTeo <2603728@sit.singaporetech.edu.sg>
2026-09-09 15:28:19 +0800

    Activity 2

--- hello.py
+++ hello.py
@@ -1,22 +1,22 @@
 # Activity 1
-print("================================")
-print("Welcome Here")
-print("My first post!")
-print("================================")
-
-# Activity 2
 # print("================================")
 # print("Welcome Here")
 # print("My first post!")
 # print("================================")
 
-# username = "cool_creator"
-# bio = "Fun Blogger"
-# followers = 100
+# Activity 2
+print("================================")
+print("Welcome Here")
+print("My first post!")
+print("================================")
 
-# print("Username:", username)
-# print("Bio:", bio)
-# print("Followers:", followers)
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
 
 # Activity 3
 # followers = 100
